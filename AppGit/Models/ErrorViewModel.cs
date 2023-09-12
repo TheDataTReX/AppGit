ab2a04b87256851e974c6d939585de086e34c215
usuario1 <usuario1@DESKTOP-JADRB9J>
2023-09-12 14:57:30 -0300

    Agregar archivos de proyecto.

---
+++ AppGit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
